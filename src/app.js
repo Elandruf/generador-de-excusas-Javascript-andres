5d877e17e858a86f5230b92cdac1b9b18addaa22
Elandruf <andreseduardo2710@gmail.com>
2024-09-04 09:26:06 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -8,4 +8,24 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let nombre = ["Mi gato", "Mi mama", "Andres", "Mi papa"];
+  let verbo = ["Corrio", "Escapo", "Se durmio", "Se fue"];
+  let predicado = [
+    "En el hospital",
+    "En la escuela",
+    "En la casa",
+    "En la calle"
+  ];
+
+  let nombreIndex = Math.floor(Math.random() * nombre.length);
+  let verboIndex = Math.floor(Math.random() * verbo.length);
+  let predicadoIndex = Math.floor(Math.random() * predicado.length);
+
+  document.querySelector("#excuse").innerHTML =
+    nombre[nombreIndex] +
+    " " +
+    verbo[verboIndex] +
+    " " +
+    predicado[predicadoIndex];
 };
